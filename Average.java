174db29963c52d39bd02db530bd2c4b04cfe1022
PrabhuYaswanth <prabhuyaswanth7@gmail.com>
2025-08-20 18:16:40 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Average {
+   public static void main(String args[]) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter mark1:");
+    int m1=sc.nextInt();
+    System.out.println("Enter mark2:");
+    int m2=sc.nextInt();
+    System.out.println("Enter mark3:");
+    int m3=sc.nextInt();
+    int avg=m1+m2+m3;
+    System.out.println("Average mark is:"+avg);
+
+   }
+}
