ee82aecb7b82846f06c28f6a6dc901e40ebef5a2
PrabhuYaswanth <prabhuyaswanth7@gmail.com>
2025-08-20 18:42:38 +0530

    Add files via upload

---
+++ N_numbers.java
@@ -0,0 +1,9 @@
+public class N_numbers {
+    public static void main(String args[]){
+        int n=10;
+        System.out.print("Displaying the N natural number:");
+        for(int i=0;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
